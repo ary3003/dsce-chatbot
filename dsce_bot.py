fbe6d0b3a413160ea16e1c534404c06c36c3883c
aryan <aryan5416@gmail.com>
2017-03-08 21:28:56 +0530

    button and quickreplies

--- dsce_bot.py
+++ dsce_bot.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 WIT_TOKEN = os.environ.get('3CBB7JHIEZC66HBN2Y4D56CU6EWCCEQC')
 FB_VERIFY_TOKEN = "my_voice_is_my_password_verify_me"
-client = Wit(access_token=WIT_TOKEN, actions = actions)
 
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
@@ -25,79 +24,151 @@ def handle_verification():
     print "Verification failed!"
     return 'Error, wrong validation token'
 
-
-
 @app.route('/', methods=['POST'])
 def handle_messages():
   print "Handling Messages"
   payload = request.get_data()
   print payload
-  if payload['object'] == 'page':
-    for entry in payload['entry']:
-      messages = entry['messaging']
-      if messages[0]:
-        message = messages[0]
-        fb_id = message['sender']['id']
-        text = message['message']['text']
-        client.run_actions(session_id = fb_id, message=text)
+  for payload_events in messaging_events(payload):
+    if sender in payload_events and message in payload_events:
+      print "Incoming from %s: %s" % (sender, message)
+      if message == "What can I ask you?":
+        quick_reply(PAT, sender, message)
       else:
-        return 'Received another event'
-  return None
+        send_message(PAT, sender, message)
+  return "ok"
 
-def first_entity_value(entities, entity):
-    """
-    Returns first entity value
-    """
-    if entity not in entities:
-        return None
-    val = entities[entity][0]['value']
-    if not val:
-        return None
-    return val['value'] if isinstance(val, dict) else val
+
+
+
+
+      
+
+def messaging_events(payload):
+  """Generate tuples of (sender_id, message_text) from the
+  provided payload.
+  """
+  data = json.loads(payload)
+  messaging_events = data["entry"][0]["messaging"]
+  for event in messaging_events:
+      if "message" in event and  "text" in event["message"]:
+        yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+      if "payload" in event['message']['quick_reply']:
+        yield event['quick_reply']['payload']
+
+
+def function(token, user):
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%token1,
+      data = json.dumps({
+        "recipient": {"id": user},
+        "message": {
+        "text": "You selected OPTION1"},
+        }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+        
+  
+"""
+def postback_events(payload):
+  data = json.loads(payload)
+  postback_events = data["entry"][0]["postback"]
+  for event in postback_events:
+    yield postback["postback"]["payload"]
   
 
-def fb_message(sender_id, text):
-  data = {
-      'recipient':{'id': sender_id},
-      'message': {'text': text}
-      }
-
-  qs = 'access_token=' + PAT
-  resp = requests.post('https://graph.facebook.com/v2.6/me/messages?' + qs, json=data)
-  if resp.status_code != requests.codes.ok:
+def postback_received(token1, postback):
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%token1,
+      data = json.dumps({
+        "recipient": {"id": user1},
+        "message": {
+        "text": "You can ask: ",
+        "quick_replies":[
+          {
+            "content_type":"text",
+            "title":"Option1",
+            "payload":"OPTION1_PAYLOAD"
+            },
+            {
+              "content_type":"text",
+              "title":"Option2",
+              "payload":"OPTION2_PAYLOAD"
+            }
+          ]
+        },
+          "postback": {
+          "payload": "GET_STARTED_PAYLOAD"
+          }
+          
+        
+        }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
     print r.text
-  return resp.content
 
-def send(request, response):
+    """
 
-  fb_id = request['context']
-  text = response['text']
-  fb_message(fb_id, text)
+    
+                      
+          
+        
+    
+    
 
-def get_forecast(request):
-    context = request['context']
-    entities = request['entities']
-    loc = first_entity_value(entities, 'location')
-    if loc:
-        # This is where we could use a weather service api to get the weather.
-        context['forecast'] = 'sunny'
-        if context.get('missingLocation') is not None:
-            del context['missingLocation']
-    else:
-        context['missingLocation'] = True
-        if context.get('forecast') is not None:
-            del context['forecast']
-    return context
+def quick_reply(access, user, text1):
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%access,
+    data=json.dumps({
+      "recipient": {"id": user},
+      "message": {
+        "text": "You can ask: ",
+        "quick_replies":[
+          {
+            "content_type":"text",
+            "title":"Option1",
+            "payload":"DEVELOPER_DEFINED_PAYLOAD"
+            },
+            {
+              "content_type":"text",
+              "title":"Option2",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+            }
+          ]
+        }
+      }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+      
 
-actions = {
-  'send': send,
-  'getForecast': get_forecast,
 
-  }
+def send_message(token, recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
 
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%token,
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {
+        "attachment":{
+          "type":"template",
+             "payload":{
+               "template_type":"button",
+               "text":"Need further assistance? Talk to our representative",
+               "buttons":[
+                 {
+                   "type":"phone_number",
+                   "title":"Call our Admission department",
+                   "payload":"+919972971606"
+                   }
+                 ]
 
-
- 
+               }
+          }
+        }
+      }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
 
 if __name__ == '__main__':
   app.run()
