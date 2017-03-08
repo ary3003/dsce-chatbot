9c52ac688e72c9f5cbe58477d42cd31af2db228e
aryan <aryan5416@gmail.com>
2017-03-08 22:03:21 +0530

    button and quickreplies

--- dsce_bot.py
+++ dsce_bot.py
@@ -34,7 +34,7 @@ def handle_messages():
     if message == "What can I ask you?":
       quick_reply(PAT, sender, message)
     else:
-        send_message(PAT, sender, message)
+      send_message(PAT, sender, message)
   return "ok"
 
 
