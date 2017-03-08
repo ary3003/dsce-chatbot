c1e60a9a30dab5534fc495272aef1bf12a1b1912
aryan <aryan5416@gmail.com>
2017-03-08 21:43:11 +0530

    wit.ai added

--- dsce_bot.py
+++ dsce_bot.py
@@ -1,174 +1,131 @@
-from flask import Flask, request
-import json
-import requests
 import os
+import requests
 from sys import argv
 from wit import Wit
+from bottle import Bottle, request, debug
 
-app = Flask(__name__)
 
 WIT_TOKEN = os.environ.get('3CBB7JHIEZC66HBN2Y4D56CU6EWCCEQC')
-FB_VERIFY_TOKEN = "my_voice_is_my_password_verify_me"
 
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
-PAT = 'EAACVBMhfty8BANvGO7uX6JUgu2b42dbDMmiu6dDumN6VUZCiILyXFQy1cbEcaPdAomAdEPPeXmuOLD2OjnNaq25wIMzcvasfwols5ZAsGTLx7ZBRZBFlh1lS8x2rIbaMZB28l9U1Bu8bZB7ZCgsUpNfYCZCQZC9UZC3CVoftfAHoe8zgZDZD'
+FB_PAGE_TOKEN = os.environ.get('EAACVBMhfty8BABbHMlBgoNbIdkElGbVSWdaPmWM6TIaTVmSIJFHGONNgXbzuLFD8suhOKgbLQkDQdVju5TJYx0Jiq4LQc3RvFkAWEHTfni62I4jrBMP5MjcKRkDqlk1eI8imZB6visofu5ZAQKAfvBnYbsKHkAz2pEcXs0owZDZD')
 
-@app.route('/', methods=['GET'])
-def handle_verification():
-  print "Handling Verification."
-  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-    print "Verification successful!"
-    return request.args.get('hub.challenge', '')
-  else:
-    print "Verification failed!"
-    return 'Error, wrong validation token'
+FB_VERIFY_TOKEN = os.environ.get('my_voice_is_my_password_verify_me')
 
-@app.route('/', methods=['POST'])
-def handle_messages():
-  print "Handling Messages"
-  payload = request.get_data()
-  print payload
-  for payload_events in messaging_events(payload):
-    if sender in payload_events and message in payload_events:
-      print "Incoming from %s: %s" % (sender, message)
-      if message == "What can I ask you?":
-        quick_reply(PAT, sender, message)
-      else:
-        send_message(PAT, sender, message)
-  return "ok"
+# Setup Bottle Server
+debug(True)
+app = Bottle()
 
 
-
-
-
-      
-
-def messaging_events(payload):
-  """Generate tuples of (sender_id, message_text) from the
-  provided payload.
-  """
-  data = json.loads(payload)
-  messaging_events = data["entry"][0]["messaging"]
-  for event in messaging_events:
-      if "message" in event and  "text" in event["message"]:
-        yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-      if "payload" in event['message']['quick_reply']:
-        yield event['quick_reply']['payload']
-
-
-def function(token, user):
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%token1,
-      data = json.dumps({
-        "recipient": {"id": user},
-        "message": {
-        "text": "You selected OPTION1"},
-        }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.text
-        
-  
-"""
-def postback_events(payload):
-  data = json.loads(payload)
-  postback_events = data["entry"][0]["postback"]
-  for event in postback_events:
-    yield postback["postback"]["payload"]
-  
-
-def postback_received(token1, postback):
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%token1,
-      data = json.dumps({
-        "recipient": {"id": user1},
-        "message": {
-        "text": "You can ask: ",
-        "quick_replies":[
-          {
-            "content_type":"text",
-            "title":"Option1",
-            "payload":"OPTION1_PAYLOAD"
-            },
-            {
-              "content_type":"text",
-              "title":"Option2",
-              "payload":"OPTION2_PAYLOAD"
-            }
-          ]
-        },
-          "postback": {
-          "payload": "GET_STARTED_PAYLOAD"
-          }
-          
-        
-        }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.text
-
+# Facebook Messenger GET Webhook
+@app.get('/webhook')
+def messenger_webhook():
     """
-
-    
-                      
-          
-        
-    
-    
-
-def quick_reply(access, user, text1):
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%access,
-    data=json.dumps({
-      "recipient": {"id": user},
-      "message": {
-        "text": "You can ask: ",
-        "quick_replies":[
-          {
-            "content_type":"text",
-            "title":"Option1",
-            "payload":"DEVELOPER_DEFINED_PAYLOAD"
-            },
-            {
-              "content_type":"text",
-              "title":"Option2",
-              "payload":"DEVELOPER_DEFINED_PAYLOAD"
-            }
-          ]
-        }
-      }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.text
-      
+    A webhook to return a challenge
+    """
+    verify_token = request.query.get('hub.verify_token')
+    # check whether the verify tokens match
+    if verify_token == FB_VERIFY_TOKEN:
+        # respond with the challenge to confirm
+        challenge = request.query.get('hub.challenge')
+        return challenge
+    else:
+        return 'Invalid Request or Verification Token'
 
 
-def send_message(token, recipient, text):
-  """Send the message text to recipient with id recipient.
-  """
+# Facebook Messenger POST Webhook
+@app.post('/webhook')
+def messenger_post():
+    """
+    Handler for webhook (currently for postback and messages)
+    """
+    data = request.json
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            # get all the messages
+            messages = entry['messaging']
+            if messages[0]:
+                # Get the first message
+                message = messages[0]
+                # Yay! We got a new message!
+                # We retrieve the Facebook user ID of the sender
+                fb_id = message['sender']['id']
+                # We retrieve the message content
+                text = message['message']['text']
+                # Let's forward the message to the Wit.ai Bot Engine
+                # We handle the response in the function send()
+                client.run_actions(session_id=fb_id, message=text)
+    else:
+        # Returned another event
+        return 'Received Different Event'
+    return None
 
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"%token,
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {
-        "attachment":{
-          "type":"template",
-             "payload":{
-               "template_type":"button",
-               "text":"Need further assistance? Talk to our representative",
-               "buttons":[
-                 {
-                   "type":"phone_number",
-                   "title":"Call our Admission department",
-                   "payload":"+919972971606"
-                   }
-                 ]
 
-               }
-          }
-        }
-      }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.text
+def fb_message(sender_id, text):
+    """
+    Function for returning response to messenger
+    """
+    data = {
+        'recipient': {'id': sender_id},
+        'message': {'text': text}
+    }
+    # Setup the query string with your PAGE TOKEN
+    qs = 'access_token=' + FB_PAGE_TOKEN
+    # Send POST request to messenger
+    resp = requests.post('https://graph.facebook.com/me/messages?' + qs,
+                         json=data)
+    return resp.content
+
+
+def first_entity_value(entities, entity):
+    """
+    Returns first entity value
+    """
+    if entity not in entities:
+        return None
+    val = entities[entity][0]['value']
+    if not val:
+        return None
+    return val['value'] if isinstance(val, dict) else val
+
+
+def send(request, response):
+    """
+    Sender function
+    """
+    # We use the fb_id as equal to session_id
+    fb_id = request['session_id']
+    text = response['text']
+    # send message
+    fb_message(fb_id, text)
+
+
+def get_forecast(request):
+    context = request['context']
+    entities = request['entities']
+    loc = first_entity_value(entities, 'location')
+    if loc:
+        # This is where we could use a weather service api to get the weather.
+        context['forecast'] = 'sunny'
+        if context.get('missingLocation') is not None:
+            del context['missingLocation']
+    else:
+        context['missingLocation'] = True
+        if context.get('forecast') is not None:
+            del context['forecast']
+    return context
+
+# Setup Actions
+actions = {
+    'send': send,
+    'getForecast': get_forecast,
+}
+
+# Setup Wit Client
+client = Wit(access_token=WIT_TOKEN, actions=actions)
 
 if __name__ == '__main__':
-  app.run()
+    # Run Server
+    if os.environ.get('APP_LOCATION') == 'heroku':
+      run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    else:
+      run(host='localhost', port=8080, debug=True)
