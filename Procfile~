908888a21718971c5d18e72d56ab0ac53ed8547f
aryan <aryan5416@gmail.com>
2017-03-08 13:21:00 +0530

    error fixed

---
+++ Procfile~
@@ -0,0 +1,1 @@
+web: gunicorn dsce_bot:app
--- dsce_bot.py
+++ dsce_bot.py
@@ -45,20 +45,12 @@ def send_message(token, recipient, text):
   """Send the message text to recipient with id recipient.
   """
 
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=%s"token,
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
     data=json.dumps({
       "recipient": {"id": recipient},
-      "message": {"text": text.encode("unicode_escape"),
-                  "quick_replies":[
-                    {
-                      "content_type":"text",
-                      "title":"Option1"
-                    },
-                    {
-                      "content_type":"text",
-                      "title":"Option2"
-                      }]
-    }}),
+      "message": {"text": text.decode('unicode_escape'),}
+    }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
     print r.text
