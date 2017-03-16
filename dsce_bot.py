db992dbdebae564692b3d098e918a8e608117cf0
aryan <aryan5416@gmail.com>
2017-03-17 01:25:45 +0530

    api.ai

--- dsce_bot.py
+++ dsce_bot.py
@@ -12,14 +12,15 @@ ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def verify():
-    # our endpoint echos back the 'hub.challenge' value specified when we setup the webhook
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == '560078':
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
+def handle_verification():
+    print "Handling Verification."
+    if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+        print "Verification successful!"
+        return request.args.get('hub.challenge', '')
+    else:
+        print "Verification failed!"
+        return 'Error, wrong validation token'
 
-    return 'Hello World (from Flask!)', 200
 
 def reply(user_id, msg):
     data = {
